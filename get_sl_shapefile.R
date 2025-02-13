68d8a93f8319b06a407b9c26cb5cd3688b426502
gabrielbattcock@gmail.com <gabrielbattcock@gmail.com>
2025-02-13 09:59:50 +0100

    playing with some shapefiles

--- get_sl_shapefile.R
+++ get_sl_shapefile.R
@@ -31,7 +31,7 @@ source_url("https://raw.githubusercontent.com/MIMI-wfp/MIMI-R-functions/refs/hea
 
 # ------------------------------------------------------------------------------
 
-lka_adm1 <- get_shapefile(adm0_code = 231, level = 'adm2')
+lka_adm1 <- get_shapefile(adm0_code = 231, level = 'adm1')
 
 lka_adm1 <- lka_adm1 %>% 
   mutate(country = "Sri Lanka")
@@ -56,12 +56,30 @@ tm_shape(india_adm1) +
   tm_shape(lka_adm1) +
   tm_fill(col  = "country") +
   tm_layout(frame =F)+
-  tm_borders(col= 'black', lwd = 1) +
-  tm_shape(pka_adm1)+
+  tm_borders(col= 'black', lwd = 1) 
+  
+  
+bga_adm1 <- get_shapefile(adm0_code = 23, level = 'adm1') %>% 
+  mutate(country = "Bangladesh")
+
+ch_adm1 <- get_shapefile(adm0_code = 53, level = 'adm1') %>% 
+  mutate(country = "China")
+
+jammu <- get_shapefile(adm0_code = 40781, level = 'adm1') %>% 
+  mutate(country = "Jammu and Kashmir")
+
+aru <- get_shapefile(adm0_code = 15, level = 'adm1') %>% 
+  mutate(country = "Arunachal Pradesh")
+
+nep_adm1 <- get_shapefile(adm0_code = 175, level = 'adm1') %>% 
+  mutate(country = "Nepal")
+
+region_adm1 <- bind_rows(bga_adm1, india_adm1,pka_adm1,lka_adm1,ch_adm1,jammu,aru,nep_adm1)
+
+  
+tm_shape(region_adm1)+
   tm_fill(col = 'country')+
   tm_borders(col = "black")
 
 
 
-tm_shape(hsee_adm1)+
-  tm_fill(col = "Name")
