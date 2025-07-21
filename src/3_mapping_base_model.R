ab17ff8410f6dbde919e65d2d275e07c9bd89814
gabrielbattcock@gmail.com <gabrielbattcock@gmail.com>
2025-07-21 14:50:43 +0200

    finding out percentage of hh consuming foods that need conversion from non-standard units

--- src/3_mapping_base_model.R
+++ src/3_mapping_base_model.R
@@ -11,19 +11,19 @@ base_ai <- read_rds("data/processed/base_ai.RDS")
 
 # connect to database
 
-# 
-# con <- DBI::dbConnect(RMySQL::MySQL(),
-#                  dbname = Sys.getenv("DB_NAME"),
-#                  host = "127.0.0.1",
-#                  port = 3306,
-#                  user = Sys.getenv("DB_USER"),
-#                  password =  Sys.getenv("DB_PASSWORD"))
-# 
-# 
-# # collect information from database
-# 
-# h_ar <- DBI::dbReadTable(con, "h_ar")
-# # 
+
+con <- DBI::dbConnect(RMySQL::MySQL(),
+                 dbname = Sys.getenv("DB_NAME"),
+                 host = "127.0.0.1",
+                 port = 3306,
+                 user = Sys.getenv("DB_USER"),
+                 password =  Sys.getenv("DB_PASSWORD"))
+
+
+# collect information from database
+
+h_ar <- DBI::dbReadTable(con, "h_ar")
+#
 
 
 # # disconnect
@@ -245,32 +245,6 @@ all_plots$adm2_fe  # ADM2 iron plot
 
 
 ################################################################################
-
-climate_adm2 <- read_csv("data/climate_features_lka_19.csv")
-
-climate_adm2 <- climate_adm2 %>%
-  left_join(hh_info %>% 
-              select(hhid,adm2) %>% 
-              mutate(hhid = as.numeric(hhid)),
-            by = c("household_id" = "hhid")) %>% 
-  group_by(adm2) %>% 
-  summarise(r3q = mean(r3q), rfh_avg = mean(rfh_avg), vim_avg = mean(vim_avg)) 
-  # slice(1) %>% 
-  # select(-household_id)
-  
-
-# climate variables versus risk
-
-adm2_inad <- adm2_average %>% 
-  select(adm2, energy_kcal_q50,
-         ends_with("_inad")) %>% 
-  left_join(climate_adm2, by = 'adm2')
-
-
-adm2_inad %>% 
-  mutate(state = factor(round(as.numeric(adm2)/10))) %>% 
-  ggplot(aes(x = folate_inad,y = r3q, color = state))+
-    geom_point(aes(size = 2))
 
 
 
---
+++ src/6_climate_variables.R
@@ -0,0 +1,31 @@
+source("Src/3_mapping_base_model.R")
+
+
+
+
+climate_adm2 <- read_csv("data/climate_features_lka_19.csv")
+
+climate_adm2 <- climate_adm2 %>%
+  left_join(hh_info %>% 
+              select(hhid,adm2) %>% 
+              mutate(hhid = as.numeric(hhid)),
+            by = c("household_id" = "hhid")) %>% 
+  group_by(adm2) %>% 
+  summarise(r3q = mean(r3q), rfh_avg = mean(rfh_avg), vim_avg = mean(vim_avg)) 
+# slice(1) %>% 
+# select(-household_id)
+
+
+# climate variables versus risk
+
+adm2_inad <- adm2_average %>% 
+  select(adm2, energy_kcal_q50,
+         ends_with("_inad")) %>% 
+  left_join(climate_adm2, by = 'adm2')
+
+
+adm2_inad %>% 
+  mutate(state = factor(round(as.numeric(adm2)/10))) %>% 
+  ggplot(aes(x = folate_inad,y = r3q, color = state))+
+  geom_point(aes(size = 2))
+
